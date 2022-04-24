169576ba07fb6efe69325c6124705d94b12aa316
yanpeiqi <1305428363@qq.com>
2022-04-24 17:14:55 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/neusoft/ypq/a2_imagegroup/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.neusoft.ypq.a2_imagegroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
